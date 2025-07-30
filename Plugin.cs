372ada4ab8fa3d44e06f03062f595d67cabe2404
duong7 <duong70g@gmail.com>
2025-07-30 14:40:25 +0700

    1st upload

---
+++ Plugin.cs
@@ -0,0 +1,83 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace NITWCustomSpeechMod;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("Night In The Woods.exe")]
+public class Plugin : BaseUnityPlugin
+{
+    public static ConfigEntry<string> TriggerKey;
+    public static ConfigEntry<string> CustomQuotes;
+    public static uint CurrentQuoteIndex = 0;
+
+    public static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} loaded.");
+
+        TriggerKey = Config.Bind(
+            "NITWCustomSpeechMod", 
+            "TriggerKey", 
+            "j", 
+            "Key that when pressed will display your custom speech bubble, e.g. a, 1, f1, insert, left ctrl, right"
+        );
+        CustomQuotes = Config.Bind(
+            "NITWCustomSpeechMod", 
+            "Quotes", 
+            $"Uhh...can someone prompt me my line? (BepInEx/config/{MyPluginInfo.PLUGIN_GUID}.cfg, 'Quotes' entry)", 
+            "Quotes for Mae to say"
+        );
+
+        var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll(typeof(AddPlayerCustomDialogPatch));
+    }
+}
+
+[HarmonyPatch(typeof(Player))]
+static class AddPlayerCustomDialogPatch 
+{
+    [HarmonyPostfix]
+    [HarmonyPatch("Update")]
+    static void Update_Postfix()
+    {
+        try
+        {
+            if (Input.GetKeyDown(Plugin.TriggerKey.Value.ToLower()))
+            {
+                SpeechBubble.CloseAll();
+
+                // global::UnityEngine.Object.Instantiate<SpeechBubble>(Global.prefabs.speechBubbles[0])
+                // .Say()
+                SpeechBubble.CreateSpeechBubble(
+                    Global.dialogue,
+                    SpeechBubble.Type.Normal,
+                    Global.player.GetComponent<Character>(),
+                    true,
+                    Plugin.CustomQuotes.Value.Split(';')[Plugin.CurrentQuoteIndex],
+                    0
+                );
+
+                if (Plugin.CurrentQuoteIndex >= Plugin.CustomQuotes.Value.Length - 1)
+                {
+                    Plugin.CurrentQuoteIndex = 0;
+                }
+                else
+                {
+                    Plugin.CurrentQuoteIndex += 1;
+                }
+            }
+        } 
+        catch (Exception err)
+        {
+            Debug.LogError($"{MyPluginInfo.PLUGIN_GUID} ERROR: {err}");
+            Plugin.Logger.LogError($"{MyPluginInfo.PLUGIN_GUID} ERROR: {err}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# NITWCustomSpeechMod
+Simple BepInEx plug-in for *Night In The Woods*, a game by Infinite Fall. With this plug-in, Mae can say user-defined quotes when a pre-configured trigger key is pressed.
+## Prerequisites
+- Base game
+- `BepInEx` (tested on version `5.4.23.3`)
+- `.NET SDK >= 8.0` if building from source.
+## Installation and usage
+- Build the plug-in from source with `dotnet build`, or download the pre-built one if available 
+- Put this plug-in folder inside `<GAME_ROOT_DIRECTORY>\BepInEx\plugins`
+- Run the game to test whether this plug-in is working correctly. The default trigger key is `j`
+- Quotes and trigger key for quotes can be configured by modifying respective entries in `<GAME_ROOT_DIRECTORY>\BepInEx\config\nxhd.nitwcustomspeechmod.cfg`. Individual quotes should be separated by `;`, and trigger key should be in `lowercase`
+- Errors, if there are any, are logged in `<GAME_ROOT_DIRECTORY>\BepInEx\LogOutput.log`.
+## Tools used during plugin development
+- `BepInEx` with `UnityExplorer` plugin
+- `dnSpy`
+- `AssetStudio`
+- `NITW Dialogue Tool`.
